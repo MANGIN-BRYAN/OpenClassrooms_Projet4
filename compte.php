ba5d2a4bd52bbfa46784d898ed0fc5130a43a672
MANGIN Bryan <averynewlife@gmail.com>
2018-08-04 20:54:42 +0200

    Multiples changements
    Balises <link> : police de caractère, Poppins
    <section id="compte">
    
    Ajout du pied de page dans un include :
    <?php include "footer.php" ?>

--- compte.php
+++ compte.php
@@ -20,12 +20,17 @@ $commentaires = commentaires_user();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Billet simple pour l'Alaska - Compte</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,300,700">
-    <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/main.css">
 </head>
 <body>
     <?php include "header.php" ?>
-    <div class="container commentaires">
+          <!--==========================
+    Section Hero
+  ============================-->
+  <section id="compte">
+    <div class="hero-container">
         <div class="row">
             <div class="col-xs-12">
                 <h1>Bienvenue <?= $infos["pseudo"] ?></h1>
@@ -53,13 +58,14 @@ $commentaires = commentaires_user();
         <?php
         endforeach;
         ?>
-        <footer>
-            <div class="row">
-                <div class="col-xs-12">
-                    <a href="contact.php">Contact</a> - <a href="mentions.php">Mentions légales</a> - <a href="https://www.facebook.com/infoprog.tuto">Facebook</a>
-                </div>
-            </div>
-        </footer>
     </div>
+  </section><!-- #hero -->
+  <?php include "footer.php" ?>
+      <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+    <script type="text/javascript">
+        jQuery(document).ready(function( $ ) {
+            jQuery(".account").addClass('menu-active');
+        });
+    </script>
 </body>
 </html>
